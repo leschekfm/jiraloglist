2f23c5f7b88c31b59a845a0db6e4ba0e4c6add93
leschekfm <leschekfm@web.de>
2016-12-01 11:46:44 +0100

    use debug and exclude logs that weren't submitted on the specified day

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const request = require('request')
 const moment = require('moment')
+const debug = require('debug')('jiraloglist')
 
 const baseUrl = "https://maloon.atlassian.net/rest/api/2/"
 
@@ -12,22 +13,25 @@ const req = request.defaults({
 })
 
 let tracking = {}
+const yesterday = moment().startOf('day').subtract(1, 'd')
+const dayToCheck = yesterday // will get an option to be overriden by a param
 
 req.get({
     uri: 'search',
     qs: {
-        //since: moment().subtract(1, 'd').valueOf(),
-        jql: "updated > 2016-11-30 and project in (CMD) and timespent > 0",
+        jql: `updated > ${dayToCheck.format('YYYY-MM-DD')} and project in (CMD) and timespent > 0`,
         fields: "summary,worklog",
         maxResults: 1000
     }
 }, (err, res, body) => {
     const response = JSON.parse(body)
-    
+
     for (issue of response.issues) {
         for (log of issue.fields.worklog.worklogs) {
-            if (moment(log.created).isAfter('2016-11-30')) {
-                //console.log(`${log.author.name} Logged ${log.timeSpent} on issue ${issue.fields.summary}`)
+            const created = moment(log.created)
+            debug('log entry created', created)
+            if (created.isAfter(dayToCheck) && created.isBefore(moment(dayToCheck).add(1,'d'))) {
+                debug(`${log.author.name} Logged ${log.timeSpent} on issue ${issue.fields.summary}`)
                 if (!(log.author.name in tracking)) {
                     tracking[log.author.name] = {}
                 }
@@ -38,16 +42,16 @@ req.get({
                     tracking[log.author.name][issue.key].timeSpent += log.timeSpentSeconds
                 }
             }
-            //console.log(`${log.author.name} Logged ${log.timeSpent} on issue ${issue.fields.summary}`)
+            debug(`${log.author.name} Logged ${log.timeSpent} on issue ${issue.fields.summary}`)
         }
     }
 
-    //console.dir(tracking)
+    debug('result object', tracking)
 
     console.log('WORKLOG')
     for (user in tracking) {
         console.log('For: ' + user)
-        for (issue in tracking[user]){
+        for (issue in tracking[user]) {
             console.log(`${issue} - ${tracking[user][issue].desc}: ${tracking[user][issue].timeSpent / 60} mins`)
         }
     }
