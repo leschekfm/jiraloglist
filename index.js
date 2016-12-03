3469a32651643158f7c47ad85802577aa9d475ed
leschekfm <leschekfm@web.de>
2016-12-03 16:28:28 +0100

    auto fix style

--- index.js
+++ index.js
@@ -7,14 +7,14 @@ program
     .option('-d, --day-to-check [dayToCheck]', 'day to check in format "YYYY-MM-DD"', moment().startOf('day').subtract(1, 'd'))
     .parse(process.argv)
 
-const baseUrl = "https://maloon.atlassian.net/rest/api/2/"
+const baseUrl = 'https://maloon.atlassian.net/rest/api/2/'
 
 const req = request.defaults({
-    auth: {
-        user: process.env.JIRAUSER,
-        pass: process.env.JIRAPASS
-    },
-    baseUrl: baseUrl
+  auth: {
+    user: process.env.JIRAUSER,
+    pass: process.env.JIRAPASS
+  },
+  baseUrl: baseUrl
 })
 
 let tracking = {}
@@ -23,45 +23,43 @@ const dayToCheck = moment(program.dayToCheck)
 debug('dayToCheck', dayToCheck)
 
 req.get({
-    uri: 'search',
-    qs: {
-        jql: `updated > ${dayToCheck.format('YYYY-MM-DD')} and project in (CMD) and timespent > 0`,
-        fields: "summary,worklog",
-        maxResults: 1000
-    }
+  uri: 'search',
+  qs: {
+    jql: `updated > ${dayToCheck.format('YYYY-MM-DD')} and project in (CMD) and timespent > 0`,
+    fields: 'summary,worklog',
+    maxResults: 1000
+  }
 }, (err, res, body) => {
-    const response = JSON.parse(body)
+  const response = JSON.parse(body)
 
-    for (issue of response.issues) {
-        for (log of issue.fields.worklog.worklogs) {
-            const created = moment(log.created)
-            debug('log entry created', created)
-            if (created.isAfter(dayToCheck) && created.isBefore(moment(dayToCheck).add(1,'d'))) {
-                debug('entry is in the specified range')
-                if (!(log.author.name in tracking)) {
-                    tracking[log.author.name] = { timeSpent: 0, issues: {} }
-                }
-                tracking[log.author.name].timeSpent += log.timeSpentSeconds
-                if (!(issue.key in tracking[log.author.name])) {
-                    tracking[log.author.name].issues[issue.key] = { desc: issue.fields.summary, timeSpent: log.timeSpentSeconds }
-                }
-                else {
-                    tracking[log.author.name].issues[issue.key].timeSpent += log.timeSpentSeconds
-                }
-            }
-            debug(`${log.author.name} Logged ${log.timeSpent} on issue ${issue.fields.summary}`)
+  for (issue of response.issues) {
+    for (log of issue.fields.worklog.worklogs) {
+      const created = moment(log.created)
+      debug('log entry created', created)
+      if (created.isAfter(dayToCheck) && created.isBefore(moment(dayToCheck).add(1, 'd'))) {
+        debug('entry is in the specified range')
+        if (!(log.author.name in tracking)) {
+          tracking[log.author.name] = { timeSpent: 0, issues: {} }
         }
-    }
-
-    debug('result object', tracking)
-
-    console.log(`WORKLOG for: ${dayToCheck.format('YYYY-MM-DD')}`)
-    for (user in tracking) {
-        const totalHours = tracking[user].timeSpent / 60 / 60
-        console.log(`${user} logged ${totalHours.toFixed(1)}h`)
-        for (issue in tracking[user].issues) {
-            console.log(`${issue} - ${tracking[user].issues[issue].desc}: ${tracking[user].issues[issue].timeSpent / 60} mins`)
+        tracking[log.author.name].timeSpent += log.timeSpentSeconds
+        if (!(issue.key in tracking[log.author.name])) {
+          tracking[log.author.name].issues[issue.key] = { desc: issue.fields.summary, timeSpent: log.timeSpentSeconds }
+        } else {
+          tracking[log.author.name].issues[issue.key].timeSpent += log.timeSpentSeconds
         }
+      }
+      debug(`${log.author.name} Logged ${log.timeSpent} on issue ${issue.fields.summary}`)
     }
+  }
 
-}) 
+  debug('result object', tracking)
+
+  console.log(`WORKLOG for: ${dayToCheck.format('YYYY-MM-DD')}`)
+  for (user in tracking) {
+    const totalHours = tracking[user].timeSpent / 60 / 60
+    console.log(`${user} logged ${totalHours.toFixed(1)}h`)
+    for (issue in tracking[user].issues) {
+      console.log(`${issue} - ${tracking[user].issues[issue].desc}: ${tracking[user].issues[issue].timeSpent / 60} mins`)
+    }
+  }
+})
