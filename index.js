9f8bffc855fefba8878190aa2a71075a5762de61
Felix Maier <leschekfm@web.de>
2017-08-03 19:17:41 +0200

    WIP post summary to slack

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const moment = require('moment')
 const program = require('commander')
 const rc = require('rc')
 
-const jiraloglist = require('./lib/jiraloglist').jll
+//const jiraloglist = require('./lib/jiraloglist').jll
+const jiraloglist = require('./lib/gitlabloglist').gll
 
 program
   .option('-d, --day-to-check [dayToCheck]', 'day to check in format "YYYY-MM-DD"', moment().startOf('day').subtract(1, 'd'))
