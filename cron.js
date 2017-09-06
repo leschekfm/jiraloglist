decec99402e977d5816afe894e6882a4d9acdcd8
leschekfm <leschekfm@web.de>
2017-09-06 07:39:41 +0200

    define daily gitlab log job

--- cron.js
+++ cron.js
@@ -7,6 +7,7 @@ const config = rc('jiraloglist', {})
 const agenda = new Agenda({db: {address: config.mongo}})
 
 const jiraloglist = require('./lib/jiraloglist').jll
+const gll = require('./lib/gitlabloglist').gll
 
 // or override the default collection name:
 // var agenda = new Agenda({db: {address: mongoConnectionString, collection: "jobCollectionName"}});
@@ -17,7 +18,7 @@ const jiraloglist = require('./lib/jiraloglist').jll
 // or pass in an existing mongodb-native MongoClient instance
 // var agenda = new Agenda({mongo: myMongoClient});
 
-agenda.define('daily worklog', function (job, done) {
+agenda.define('daily jira worklog', function (job, done) {
   let range = 1
   let start = moment().startOf('day').subtract(1, 'd').add(5, 'h')
   if (moment().day() === 1) { // on monday
@@ -29,8 +30,21 @@ agenda.define('daily worklog', function (job, done) {
   done()
 })
 
+agenda.define('daily gitlab cp2 worklog', function (job, done) {
+  let range = 1
+  let start = moment().startOf('day').subtract(1, 'd').add(5, 'h')
+  if (moment().day() === 1) { // on monday
+    start = moment().startOf('day').subtract(3, 'd').add(5, 'h')
+    range = 3
+  }
+
+  gll(config, start, range)
+  done()
+})
+
 agenda.on('ready', function () {
-  agenda.every(config.cron.interval, 'daily worklog')
+  agenda.every(config.cron.interval, 'daily jira worklog')
+  agenda.every(config.cron.interval, 'daily gitlab cp2 worklog')
 
   // Alternatively, you could also do:
   // agenda.every('*/3 * * * *', 'delete old users');
